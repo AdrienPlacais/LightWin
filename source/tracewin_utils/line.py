c5d5653548b8da9993f4f3db0895cdc40e81ed32
Adrien P <placais@lpsc.in2p3.fr>
2024-06-08 11:06:49 +0200

    bug correction

--- source/tracewin_utils/line.py
+++ source/tracewin_utils/line.py
@@ -2,6 +2,95 @@
 
 import re
 from collections.abc import Collection
+from typing import Any
+
+
+class DatLine:
+    """Hold a single line of the ``.dat`` file."""
+
+    def __init__(self, line: str, idx: int) -> None:
+        """Instantiate the object."""
+        self._original_line = line
+        self.idx = idx
+
+        self.personalized_name: str | None = None
+        self.weight: float | None = None
+        self.splitted = []
+
+        self._parse_line(line)
+
+    def __repr__(self) -> str:
+        """Give data used to create current object."""
+        return f"#{self.idx:4d} | {self.line}"
+
+    def __str__(self) -> str:
+        """Do the same thing as __repr__ for now."""
+        return self.__repr__()
+
+    def _parse_line(self, line: str) -> None:
+        """Parse the line into its components."""
+        if _is_a_path_instruction(line):
+            self.splitted = line.split()
+            return
+
+        elements = slice_dat_line(line)
+        if len(elements) == 0:
+            return
+        # Check for a personalized name
+        name_colon_index = elements[0].find(":")
+        if name_colon_index != -1:
+            self.personalized_name = elements.pop(0).strip(":")
+
+        if len(elements) <= 1:
+            self.splitted = elements
+            return
+
+        if elements[1] == ":":
+            del elements[1]
+            self.personalized_name = elements.pop(0)
+
+        if len(elements) <= 1:
+            self.splitted = elements
+            return
+
+        # Check for weight
+        if elements[1].startswith("(") and elements[1].endswith(")"):
+            self.weight = float(elements.pop(1).strip(")").strip("("))
+
+        self.splitted = elements
+
+    @property
+    def instruction(self) -> str:
+        """Return the command or element name.
+
+        Useful for :class:`.Instruction` instantiation.
+
+        """
+        return self.splitted[0].upper() if self.splitted else ""
+
+    @property
+    def line(self) -> str:
+        """Reconstruct the line (which may have changed since creation)."""
+        return " ".join(self.splitted_full)
+
+    @property
+    def splitted_full(self) -> list[str]:
+        """Give splitted line with name and weight."""
+        base = self.splitted[:]
+        if self.weight is not None:
+            base.insert(0, str(self.weight))
+        if self.personalized_name is not None:
+            base.insert(0, f"{self.personalized_name}:")
+        return base
+
+    @property
+    def n_args(self) -> int:
+        """Give number of arguments contained in line."""
+        return len(self.splitted[1:])
+
+    def change_argument(self, value: Any, position: int) -> None:
+        """Change value of an argument."""
+        self.splitted[position] = str(value)
 
 
 def _split_named_elements(elements: Collection[str]) -> list[str]:
@@ -64,76 +153,6 @@ def _is_a_path_instruction(line: str) -> bool:
         if splitted[0].casefold() == instruction.casefold():
             return True
     return False
-
-
-class DatLine:
-    """Hold a single line of the ``.dat`` file."""
-
-    def __init__(self, line: str, idx: int) -> None:
-        """Instantiate the object."""
-        self._original_line = line
-        self.idx = idx
-
-        self.personalized_name: str | None = None
-        self.weight: float | None = None
-        self.splitted = []
-
-        self._parse_line(line)
-
-    def __repr__(self) -> str:
-        """Give data used to create current object."""
-        return f"#{self.idx:4d} | {self.line}"
-
-    def __str__(self) -> str:
-        """Do the same thing as __repr__ for now."""
-        return self.__repr__()
-
-    def _parse_line(self, line: str) -> None:
-        """Parse the line into its components."""
-        if _is_a_path_instruction(line):
-            self.splitted = line.split(maxsplit=1)
-            return
-
-        elements = slice_dat_line(line)
-        if len(elements) == 0:
-            return
-        # Check for a personalized name
-        name_colon_index = elements[0].find(":")
-        if name_colon_index != -1:
-            self.personalized_name = elements.pop(0).strip(":")
-
-        if len(elements) <= 1:
-            self.splitted = elements
-            return
-
-        if elements[1] == ":":
-            del elements[1]
-            self.personalized_name = elements.pop(0)
-
-        # Check for weight
-        if elements[1].startswith("(") and elements[1].endswith(")"):
-            self.weight = float(elements.pop(1).strip(")").strip("("))
-
-        self.splitted = elements
-
-    @property
-    def instruction(self) -> str:
-        """Return the command or element name.
-
-        Useful for :class:`.Instruction` instantiation.
-
-        """
-        return self.splitted[0] if self.splitted else ""
-
-    @property
-    def line(self) -> str:
-        """Reconstruct the line (which may have changed since creation)."""
-        base = self.splitted[:]
-        if self.weight is not None:
-            base.insert(0, str(self.weight))
-        if self.personalized_name is not None:
-            base.insert(0, f"{self.personalized_name}:")
-        return " ".join(base)
 
 
 # Example usage
