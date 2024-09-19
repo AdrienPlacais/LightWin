c20d57f1955aa640492a11bb117096d3709ce53a
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-09-19 11:02:32 +0200

    a single function to gather all value edition

--- src/lightwin/new_config/toml_formatter.py
+++ src/lightwin/new_config/toml_formatter.py
@@ -1,12 +1,20 @@
 """Define several helper functions for proper ``.toml`` formatting."""
 
 import logging
-from pathlib import Path
 from typing import Any
 
 
-def str_toml(key: str, value: Any) -> str:
-    """Format the value for proper toml writing."""
+def format_for_toml(input_type: type, key: str, value: Any) -> str:
+    """Format the value so that it matches ``toml`` standard."""
+    if input_type is str:
+        return _str_toml(key, value)
+    if input_type is bool:
+        return _bool_toml(key, value)
+    return f"{value}"
+
+
+def _str_toml(key: str, value: Any) -> str:
+    """Surround value with quotation marks."""
     if not isinstance(value, str):
         try:
             value = str(value)
@@ -20,8 +28,8 @@ def str_toml(key: str, value: Any) -> str:
     return '"' + value + '"'
 
 
-def bool_toml(key: str, value: Any) -> str:
-    """Format the value for proper toml writing."""
+def _bool_toml(key: str, value: Any) -> str:
+    """Return 'true' or 'false'."""
     if not isinstance(value, bool):
         try:
             value = bool(value)
