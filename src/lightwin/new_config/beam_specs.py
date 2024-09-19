b5bde9142547de7c05161dff0699b782a752a050
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-09-19 10:09:29 +0200

    entries alphabetically sorted, sigma can be numpy array

--- src/lightwin/new_config/beam_specs.py
+++ src/lightwin/new_config/beam_specs.py
@@ -1,25 +1,21 @@
 """Define parameters necessary to define a beam."""
 
+import numpy as np
+
 from lightwin.new_config.config_specs import KeyValConfSpec
 
 BEAM_CONFIG = (
-    KeyValConfSpec(
-        key="e_rest_mev",
-        types=(float,),
-        description=r"Rest energy of particle in :math:`\mathrm{MeV}`",
-        default_value=0.0,
-    ),
-    KeyValConfSpec(
-        key="q_adim",
-        types=(float,),
-        description="Adimensioned charge of particle",
-        default_value=1.0,
-    ),
     KeyValConfSpec(
         key="e_mev",
         types=(float,),
         description=r"Energy of particle at entrance in :math:`\mathrm{MeV}",
         default_value=1.0,
+    ),
+    KeyValConfSpec(
+        key="e_rest_mev",
+        types=(float,),
+        description=r"Rest energy of particle in :math:`\mathrm{MeV}`",
+        default_value=0.0,
     ),
     KeyValConfSpec(
         key="f_bunch_mhz",
@@ -35,8 +31,14 @@ BEAM_CONFIG = (
         is_mandatory=False,
     ),
     KeyValConfSpec(
+        key="q_adim",
+        types=(float,),
+        description="Adimensioned charge of particle",
+        default_value=1.0,
+    ),
+    KeyValConfSpec(
         key="sigma",
-        types=(list,),
+        types=(list, np.ndarray),
         description=r"Input :math:`\sigma` beam matrix in :math:`\mathrm{m}`;"
         + r" :math:`\mathrm{rad}`. Must be a list of lists of floats that can "
         + "be transformed to a 6*6 matrix.",
