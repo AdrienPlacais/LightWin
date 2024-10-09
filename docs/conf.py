dd84d4665ad05fed1d57f7306479282d2c98364d
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-10-09 09:49:44 +0200

    set call to other libraries links, removed the ones that do no longer cause any issue

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx_rtd_theme",  # ReadTheDocs theme
     "myst_parser",
+    "sphinx.ext.intersphinx",  # interlink with other docs, such as numpy
     "sphinx.ext.todo",  # allow use of TODO
     # "sphinx.ext.viewcode",
     "nbsphinx",
@@ -63,40 +64,23 @@ autodoc_default_options = {
 add_module_names = False
 default_role = "literal"
 todo_include_todos = True
+
+templates_path = ["_templates"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "lightwin/modules.rst",
+    "**/*.inc.rst",
+]
+bibtex_bibfiles = ["references.bib"]
+
+# -- Check that there is no broken link --------------------------------------
 nitpicky = True
 nitpick_ignore = [
-    ("py:exc", "NotImplementedError"),
-    ("py:exc", "TypeError"),
-    ("py:class", "abc.ABCMeta"),
-    ("py:class", "ABCMeta"),
-    ("py:class", "abc.ABC"),
-    ("py:class", "cls"),
-    ("py:class", "collections.abc.Callable"),
-    ("py:class", "collections.abc.Collection"),
-    ("py:class", "collections.abc.Container"),
-    ("py:class", "collections.abc.Generator"),
-    ("py:class", "collections.abc.Iterable"),
-    ("py:class", "collections.abc.MutableSequence"),
-    ("py:class", "collections.abc.Sequence"),
-    ("py:class", "datetime.timedelta"),
-    ("py:class", "logging.Formatter"),
-    ("py:class", "matplotlib.axes._axes.Axes"),
-    ("py:class", "matplotlib.figure.Figure"),
-    ("py:class", "matplotlib.pyplot.figure"),
-    ("py:class", "matplotlib.patches.Ellipse"),
-    ("py:class", "matplotlib.patches.Rectangle"),
-    ("py:class", "matplotlib.patches.Polygon"),
-    ("py:class", "ModuleType"),
-    ("py:class", "mpl_toolkits.mplot3d.axes3d.Axes3D"),
-    ("py:class", "numpy.ndarray"),
-    ("py:class", "np.ndarray"),
+    # Not recognized by Sphinx, don't know if this is normal
     ("py:class", "optional"),
-    ("py:class", "pathlib.Path"),
-    ("py:class", "Path"),
-    ("py:class", "pandas.core.frame.DataFrame"),
-    ("py:class", "pd.DataFrame"),
-    ("py:class", "pandas.core.series.Series"),
-    ("py:class", "pd.Series"),
+    ("py:class", "T"),
     # pymoo fixes should be temporary
     ("py:class", "ElementwiseProblem"),
     ("py:class", "pymoo.core.algorithm.Algorithm"),
@@ -105,15 +89,10 @@ nitpick_ignore = [
     ("py:class", "pymoo.core.problem.ElementwiseProblem"),
     ("py:class", "pymoo.core.problem.Problem"),
     ("py:class", "pymoo.termination.default.DefaultMultiObjectiveTermination"),
-    # -------------------------------
-    ("py:class", "scipy.optimize._constraints.Bounds"),
-    ("py:class", "T"),
-    ("py:class", "types.ModuleType"),
     # Due to bad design
     ("py:class", "lightwin.failures.set_of_cavity_settings.FieldMap"),
     ("py:obj", "lightwin.failures.set_of_cavity_settings.FieldMap"),
     ("py:class", "lightwin.core.list_of_elements.helper.ListOfElements"),
-    # -----------------
     # Due to Sphinx error, https://github.com/sphinx-doc/sphinx/issues/10785
     ("py:class", "definitions_t"),
     ("py:class", "ComputeBeamPropagationT"),
@@ -125,26 +104,19 @@ nitpick_ignore = [
     ("py:class", "value_t"),
     ("py:class", "REFERENCE_T"),
     ("py:class", "STATUS_T"),
-    # -----------------
-]
-# Avoid errors: `, optional` not recognized or so
-# https://github.com/sphinx-doc/sphinx/issues/6861
-napoleon_use_param = True
-
-templates_path = ["_templates"]
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    "lightwin/modules.rst",
-    "**/*.inc.rst",
 ]
 
-bibtex_bibfiles = ["references.bib"]
+# Link to other libraries
+intersphinx_mapping = {
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
+    "python": ("https://docs.python.org/3", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "display_version": True,
