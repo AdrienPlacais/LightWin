7f1d2ff81a3bf4fe8728149c88824eb4f055c466
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-10-10 12:25:00 +0200

    added config for Envelope1D

--- src/lightwin/new_config/full_specs.py
+++ src/lightwin/new_config/full_specs.py
@@ -9,6 +9,9 @@
 import logging
 from typing import Any, Literal
 
+from lightwin.new_config.specs.beam_calculator_envelope1d_specs import (
+    ENVELOPE1D_CONFIG,
+)
 from lightwin.new_config.specs.beam_calculator_tracewin_specs import (
     TRACEWIN_CONFIG,
 )
@@ -18,7 +21,7 @@ from lightwin.new_config.specs_base_objects import TableConfSpec
 
 BEAM_CALCULATORS_CONFIGS = {
     "TraceWin": TRACEWIN_CONFIG,
-    # "Envelope1D": ENVELOPE1D_CONFIG,
+    "Envelope1D": ENVELOPE1D_CONFIG,
     # "Envelope3D": ENVELOPE3D_CONFIG,
 }
 
---
+++ src/lightwin/new_config/specs/beam_calculator_envelope1d_specs.py
@@ -0,0 +1,56 @@
+"""Define how :class:`.Envelope1D` should be configured."""
+
+from lightwin.beam_calculation.envelope_1d.envelope_1d import (
+    ENVELOPE1D_METHODS,
+)
+from lightwin.new_config.specs_base_objects import KeyValConfSpec
+
+ENVELOPE1D_CONFIG = (
+    KeyValConfSpec(
+        key="flag_cython",
+        types=(bool,),
+        description="If we should use the Cython implementation (faster).",
+        default_value=False,
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="flag_phi_abs",
+        types=(bool,),
+        description=(
+            "If the field maps phases should be absolute (no implicit "
+            "rephasing after a failure)."
+        ),
+        default_value=True,
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="method",
+        types=(str,),
+        description="Integration method.",
+        default_value="RK4",
+        allowed_values=ENVELOPE1D_METHODS,
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="n_steps_per_cell",
+        types=(int,),
+        description=(
+            "Number of integrating steps per cavity cell. Recommended values "
+            "are 40 for RK4 and 20 for leapfrog."
+        ),
+        default_value=40,
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="tool",
+        types=(str,),
+        description="Name of the tool.",
+        default_value="Envelope1D",
+        allowed_values=(
+            "Envelope1D",
+            "envelope1d",
+            "Envelope_1D",
+            "envelope_1d",
+        ),
+    ),
+)
