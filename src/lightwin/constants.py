e4d61ead50e1b852f81aa3da82be3c97194d2c7c
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-10-10 09:32:30 +0200

    added path to tests folder

--- src/lightwin/constants.py
+++ src/lightwin/constants.py
@@ -7,6 +7,7 @@ c = 2.99792458e8
 
 # Folders
 example_folder = Path(__file__).absolute().parents[2] / "data/example/"
+test_folder = Path(__file__).absolute().parents[2] / "tests/"
 # Files
 example_config = example_folder / "lightwin.toml"
 example_dat = example_folder / "example.dat"
