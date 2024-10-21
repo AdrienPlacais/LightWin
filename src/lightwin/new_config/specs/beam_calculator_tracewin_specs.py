8aa858c58540162d7ec63777d598c8158c3fbad2
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-10-21 15:24:25 +0200

    readded some tw arguments

--- src/lightwin/new_config/specs/beam_calculator_tracewin_specs.py
+++ src/lightwin/new_config/specs/beam_calculator_tracewin_specs.py
@@ -18,6 +18,7 @@ from lightwin.constants import example_ini, example_machine_config
 from lightwin.new_config.key_val_conf_spec import KeyValConfSpec
 
 TRACEWIN_CONFIG = (
+    # ======================== pure TraceWin ==================================
     KeyValConfSpec(
         key="ini_path",
         types=(str, Path),
@@ -28,9 +29,51 @@ TRACEWIN_CONFIG = (
     KeyValConfSpec(
         key="hide",
         types=(bool,),
-        description="Provide this flag to forbid the GUI from showing up.",
+        description="Hide the GUI, or cancel console output (no parameter).",
         default_value=True,
     ),
+    KeyValConfSpec(
+        key="nbr_thread",
+        types=(int,),
+        description="Set the max. number of core/thread used",
+        default_value=8,
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="partran",
+        types=(int, bool),
+        description="To activate/deactivate patran tracking.",
+        default_value=0,
+        allowed_values=(0, 1, True, False),
+        is_mandatory=False,
+    ),
+    KeyValConfSpec(
+        key="synoptic_file",
+        types=(str, Path),
+        description="Save the geometric layout at (entance (=1), middle (=2), exit (=3) of elements. (See “Synoptic” tools for file name).",
+        default_value=example_ini.with_stem(".syn"),
+        is_a_path_that_must_exists=False,
+        is_mandatory=False,
+        warning_message="Not sure of this argument meaning.",
+    ),
+    KeyValConfSpec(
+        key="tab_file",
+        types=(str, Path),
+        description="Save to file the data sheet at the end of calcul (by default in calculation directory).",
+        default_value=example_ini.with_stem(".tab"),
+        is_a_path_that_must_exists=False,
+        is_mandatory=False,
+        warning_message="Not sure of this argument meaning.",
+    ),
+    KeyValConfSpec(
+        key="upgrade",
+        types=(str,),
+        description="To update LightWin",
+        default_value="",
+        is_mandatory=False,
+        error_message="Upgrading TraceWin from LightWin is a bad idea.",
+    ),
+    # ======================== LightWin =======================================
     KeyValConfSpec(
         key="machine_config_file",
         types=(str, Path),
@@ -50,14 +93,6 @@ TRACEWIN_CONFIG = (
         is_mandatory=False,
     ),
     KeyValConfSpec(
-        key="partran",
-        types=(int, bool),
-        description="To activate/deactivate patran tracking.",
-        default_value=0,
-        allowed_values=(0, 1, True, False),
-        is_mandatory=False,
-    ),
-    KeyValConfSpec(
         key="simulation_type",
         types=(str,),
         description="A key in the machine_config.toml file",
@@ -69,13 +104,5 @@ TRACEWIN_CONFIG = (
         description="Name of the tool.",
         default_value="TraceWin",
         allowed_values=("TraceWin", "tracewin"),
-    ),
-    KeyValConfSpec(
-        key="upgrade",
-        types=(str,),
-        description="To update LightWin",
-        default_value="",
-        is_mandatory=False,
-        error_message="Upgrading TraceWin from LightWin is a bad idea.",
     ),
 )
