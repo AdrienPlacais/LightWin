6ac7ea6cbdcf1f38aae6042c87ff9d7df473be3d
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-11-10 09:30:08 +0100

    moved some helpers to dedicated module

--- src/lightwin/visualization/helper.py
+++ src/lightwin/visualization/helper.py
@@ -1,5 +1,98 @@
 """Define types and helpers for the visualization library."""
 
+import logging
+from collections.abc import Sequence
+from pathlib import Path
 from typing import Literal
 
+import matplotlib.pyplot as plt
+import matplotlib.transforms as mtransforms
+import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure, SubFigure
+
 X_AXIS_T = Literal["z_abs", "elt_idx"]
+
+
+def create_fig_if_not_exists(
+    axnum: int | list[int] | range,
+    sharex: bool = False,
+    num: int = 1,
+    clean_fig: bool = False,
+    **kwargs,
+) -> tuple[Figure, list[Axes]]:
+    """
+    Check if figures were already created, create it if not.
+
+    Parameters
+    ----------
+    axnum : int | list[int] | range
+        Axes indexes as understood by ``Figure.add_subplot``, or number of
+        desired axes.
+    sharex : bool, optional
+        If x axis should be shared. The default is False.
+    num : int, optional
+        Fig number. The default is 1.
+    clean_fig: bool, optional
+        If the previous plot should be erased from Figure. The default is
+        False.
+
+    """
+    if isinstance(axnum, int):
+        # We make a one-column, `axnum` rows figure
+        axnum = range(100 * axnum + 11, 101 * axnum + 11)
+
+    if plt.fignum_exists(num):
+        fig = plt.figure(num)
+        axlist = fig.get_axes()
+        if clean_fig:
+            clean_figures([num])
+        return fig, axlist
+
+    fig = plt.figure(num)
+    axlist = [fig.add_subplot(axnum[0])]
+    shared_ax = None
+    if sharex:
+        shared_ax = axlist[0]
+    axlist += [fig.add_subplot(num, sharex=shared_ax) for num in axnum[1:]]
+    return fig, axlist
+
+
+def clean_figures(fig_ids: Sequence[int | str | Figure | SubFigure]) -> None:
+    """Clean axis of Figs in fignumlist."""
+    for fig_id in fig_ids:
+        fig = plt.figure(fig_id)
+        clean_axes(fig.get_axes())
+
+
+def clean_axes(ax_ids: Sequence[Axes]) -> None:
+    """Clean given axis."""
+    for ax in ax_ids:
+        ax.cla()
+
+
+def remove_artists(axe: Axes) -> None:
+    """Remove lines and plots, but keep labels and grids."""
+    for artist in axe.lines:
+        artist.remove()
+    axe.set_prop_cycle(None)  # type: ignore
+
+
+def _autoscale_based_on(axx: Axes, to_ignore: str) -> None:
+    """Rescale axis, ignoring Lines with to_ignore in their label."""
+    lines = [
+        line for line in axx.get_lines() if to_ignore not in line.get_label()
+    ]
+    axx.dataLim = mtransforms.Bbox.unit()
+    for line in lines:
+        datxy = np.vstack(line.get_data()).T
+        axx.dataLim.update_from_data_xy(datxy, ignore=False)
+    axx.autoscale_view()
+
+
+def savefig(fig: Figure, filepath: Path) -> None:
+    """Save the figure."""
+    fig.set_size_inches(25.6, 13.64)
+    fig.tight_layout()
+    fig.savefig(filepath)
+    logging.debug(f"Fig. saved in {filepath}")
--- src/lightwin/visualization/plot.py
+++ src/lightwin/visualization/plot.py
@@ -19,16 +19,15 @@ import itertools
 import logging
 from collections.abc import Collection
 from pathlib import Path
-from typing import Any, Callable, Literal, Sequence
+from typing import Any, Callable, Sequence
 
 import matplotlib.pyplot as plt
-import matplotlib.transforms as mtransforms
 import numpy as np
 from cycler import cycler
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 from matplotlib.typing import ColorType
-from palettable.colorbrewer.qualitative import Dark2_8
+from palettable.colorbrewer.qualitative import Dark2_8  # type: ignore
 
 import lightwin.util.dicts_output as dic
 from lightwin.beam_calculation.simulation_output.simulation_output import (
@@ -39,7 +38,11 @@ from lightwin.optimisation.objective.helper import by_element
 from lightwin.optimisation.objective.objective import Objective
 from lightwin.util import helper
 from lightwin.visualization import structure
-from lightwin.visualization.helper import X_AXIS_T
+from lightwin.visualization.helper import (
+    X_AXIS_T,
+    create_fig_if_not_exists,
+    savefig,
+)
 
 font = {"family": "serif"}  # , 'size': 25}
 plt.rc("font", **font)
@@ -185,7 +188,7 @@ def _plot_preset(
 
     if save_fig:
         file = Path(args[-1].get("accelerator_path"), f"{str_preset}.png")
-        _savefig(fig, file)
+        savefig(fig, file)
 
     return fig
 
@@ -391,13 +394,13 @@ def _compute_error(
 # Actual interface with matplotlib
 def _make_a_subplot(
     axe: Axes,
-    x_axis: X_AXIS_T | Literal["last_elt_of_sec"],
+    x_axis: X_AXIS_T,
     y_axis: str,
     colors: dict[str, str] | None,
     *accelerators: Accelerator,
     plot_section: bool = True,
     symetric_plot: bool = False,
-    **kwargs: bool | int | str,
+    **kwargs,
 ) -> None:
     """Get proper data and plot it on an Axe."""
     if plot_section:
@@ -436,92 +439,6 @@ def _make_a_subplot(
 
     axe.grid(True)
     axe.set_ylabel(_y_label(y_axis))
-
-
-# =============================================================================
-# Basic helpers
-# =============================================================================
-def create_fig_if_not_exists(
-    axnum: int | list[int],
-    sharex: bool = False,
-    num: int = 1,
-    clean_fig: bool = False,
-    **kwargs: bool | str | int,
-) -> tuple[Figure, list[Axes]]:
-    """
-    Check if figures were already created, create it if not.
-
-    Parameters
-    ----------
-    axnum : int | list[int]
-        Axes indexes as understood by fig.add_subplot or number of desired
-        axes.
-    sharex : bool, optional
-        If x axis should be shared. The default is False.
-    num : int, optional
-        Fig number. The default is 1.
-    clean_fig: bool, optional
-        If the previous plot should be erased from Figure. The default is
-        False.
-
-    """
-    if isinstance(axnum, int):
-        # We make a one-column, `axnum` rows figure
-        axnum = range(100 * axnum + 11, 101 * axnum + 11)
-
-    if plt.fignum_exists(num):
-        fig = plt.figure(num)
-        axlist = fig.get_axes()
-        if clean_fig:
-            clean_figure([num])
-        return fig, axlist
-    fig = plt.figure(num)
-    axlist = [fig.add_subplot(axnum[0])]
-    shared_ax = None
-    if sharex:
-        shared_ax = axlist[0]
-    axlist += [fig.add_subplot(num, sharex=shared_ax) for num in axnum[1:]]
-    return fig, axlist
-
-
-def clean_figure(fignumlist: Sequence[Figure]) -> None:
-    """Clean axis of Figs in fignumlist."""
-    for fignum in fignumlist:
-        fig = plt.figure(fignum)
-        clean_axes(fig.get_axes())
-
-
-def clean_axes(axlist: Sequence[Axes]) -> None:
-    """Clean given axis."""
-    for axx in axlist:
-        axx.cla()
-
-
-def remove_artists(axe: Axes) -> None:
-    """Remove lines and plots, but keep labels and grids."""
-    for artist in axe.lines:
-        artist.remove()
-    axe.set_prop_cycle(None)
-
-
-def _autoscale_based_on(axx: Axes, to_ignore: str) -> None:
-    """Rescale axis, ignoring Lines with to_ignore in their label."""
-    lines = [
-        line for line in axx.get_lines() if to_ignore not in line.get_label()
-    ]
-    axx.dataLim = mtransforms.Bbox.unit()
-    for line in lines:
-        datxy = np.vstack(line.get_data()).T
-        axx.dataLim.update_from_data_xy(datxy, ignore=False)
-    axx.autoscale_view()
-
-
-def _savefig(fig: Figure, filepath: Path) -> None:
-    """Save the figure."""
-    fig.set_size_inches(25.6, 13.64)
-    fig.tight_layout()
-    fig.savefig(filepath)
-    logging.debug(f"Fig. saved in {filepath}")
 
 
 # =============================================================================
