44e8bcd9df0dd40731c909fe8f1eda35e3488bec
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2024-11-12 13:11:17 +0100

    version number, commit number added to log

--- src/lightwin/util/log_manager.py
+++ src/lightwin/util/log_manager.py
@@ -13,8 +13,70 @@ https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 
 """
 import logging
+import subprocess
 import sys
+from importlib.metadata import PackageNotFoundError, version
 from pathlib import Path
+from typing import Literal
+
+
+def _get_package_version(package_name: str) -> str:
+    """Get the package version."""
+    try:
+        return version(package_name)
+    except PackageNotFoundError:
+        return "Unknown version"
+
+
+def _get_last_commit_hash() -> str:
+    """Get the last Git commit hash."""
+    try:
+        return (
+            subprocess.check_output(["git", "rev-parse", "HEAD"])
+            .decode("utf-8")
+            .strip()
+        )
+    except subprocess.CalledProcessError:
+        return "Unknown commit"
+
+
+def _log_header(package_name: str) -> str:
+    """Create a header for the log file."""
+    package_version = _get_package_version(package_name)
+    commit_hash = _get_last_commit_hash()
+    header_message = (
+        f"Starting log for {package_name} - Version: {package_version}, "
+        f"Commit: {commit_hash}"
+    )
+    return header_message
+
+
+def _console_handler(
+    output: str, level: str, color: bool, line_template: str
+) -> logging.Handler:
+    """Set up the console handler."""
+    output_stream = sys.stdout if output.lower() == "stdout" else sys.stderr
+    console_handler = logging.StreamHandler(output_stream)
+    console_handler.setLevel(level.upper())
+    console_formatter = LogFormatter(fmt=line_template, color=color)
+    console_handler.setFormatter(console_formatter)
+    return console_handler
+
+
+def _file_handler(
+    file: Path, level: str, color: bool, line_template: str
+) -> logging.Handler | Literal[False]:
+    """Set up the file handler."""
+    try:
+        logfile_handler = logging.FileHandler(file, mode="a")
+    except Exception as e:
+        print(f"Failed to set up log file: {e}")
+        return False
+
+    logfile_handler.setLevel(level.upper())
+    logfile_formatter = LogFormatter(fmt=line_template, color=color)
+    logfile_handler.setFormatter(logfile_formatter)
+    return logfile_handler
 
 
 class LogFormatter(logging.Formatter):
@@ -50,6 +112,7 @@ class LogFormatter(logging.Formatter):
 
 
 def set_up_logging(
+    package_name: str,
     console_log_output: str = "stdout",
     console_log_level: str = "INFO",
     console_log_color: bool = True,
@@ -68,31 +131,25 @@ def set_up_logging(
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    # Set up console handler
-    output_stream = (
-        sys.stdout if console_log_output.lower() == "stdout" else sys.stderr
+    logger.addHandler(
+        _console_handler(
+            console_log_output,
+            console_log_level,
+            console_log_color,
+            console_log_line_template,
+        )
     )
-    console_handler = logging.StreamHandler(output_stream)
-    console_handler.setLevel(console_log_level.upper())
-    console_formatter = LogFormatter(
-        fmt=console_log_line_template, color=console_log_color
-    )
-    console_handler.setFormatter(console_formatter)
-    logger.addHandler(console_handler)
 
-    # Set up file handler
-    try:
-        logfile_handler = logging.FileHandler(logfile_file, mode="a")
-    except Exception as e:
-        print(f"Failed to set up log file: {e}")
+    logfile_handler = _file_handler(
+        logfile_file,
+        logfile_log_level,
+        logfile_log_color,
+        logfile_line_template,
+    )
+    if not logfile_handler:
         return False
-
-    logfile_handler.setLevel(logfile_log_level.upper())
-    logfile_formatter = LogFormatter(
-        fmt=logfile_line_template, color=logfile_log_color
-    )
-    logfile_handler.setFormatter(logfile_formatter)
     logger.addHandler(logfile_handler)
+    logger.info(_log_header(package_name))
 
     return True
 
@@ -100,6 +157,7 @@ def set_up_logging(
 def main():
     """Main function."""
     if not set_up_logging(
+        package_name="LightWin",
         console_log_output="stdout",
         console_log_level="warning",
         console_log_color=True,
