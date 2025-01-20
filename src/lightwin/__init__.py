b8dc171bdf5848da67fe2731537d50fe86c26c97
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-01-20 09:47:37 +0100

    this time black should be applied

--- src/lightwin/__init__.py
+++ src/lightwin/__init__.py
@@ -7,3 +7,4 @@ __version__ = importlib.metadata.version("lightwin")
 missing_space = (1,2,3,4,5)
 more_than_79_characters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 more_than_88_characters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 10, 11, 12, 13, 13, 14, 15]
+
