85a6fbac697fd9814975c7a0316b311305b897ec
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-01-22 15:15:31 +0100

    quickfix for packaged data
    now I should try to package it and send it on PyPI

--- src/lightwin/constants.py
+++ src/lightwin/constants.py
@@ -1,5 +1,6 @@
 """Define constants."""
 
+from importlib import resources
 from pathlib import Path
 
 # Physics
@@ -11,12 +12,13 @@ doc_folder = _lw_base_folder / "docs/"
 example_folder = _lw_base_folder / "data/example/"
 example_results = example_folder / "results"
 test_folder = _lw_base_folder / "tests/"
-test_data_folder = test_folder / "data/"
+
 # Files
+example_folder = resources.files("lightwin.data.ads")
 example_config = example_folder / "lightwin.toml"
 example_constraints = example_folder / "constraints.csv"
-example_dat = example_folder / "example.dat"
-example_ini = example_folder / "example.ini"
+example_dat = example_folder / "ads.dat"
+example_ini = example_folder / "ads.ini"
 example_machine_config = example_folder / "machine_config.toml"
 example_variables = example_folder / "variables.csv"
 
