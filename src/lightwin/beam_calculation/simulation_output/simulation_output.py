bdf95150be94b88ba60c7e6217330d1ba1869722
aymeric tissot <aymeric.tissot@grenoble-inp.org>
2025-07-04 14:40:29 +0200

    Modified docstring

--- src/lightwin/beam_calculation/simulation_output/simulation_output.py
+++ src/lightwin/beam_calculation/simulation_output/simulation_output.py
@@ -65,7 +65,7 @@ class SimulationOutput:
         Holds energy, phase of the synchronous particle.
     cav_params :
         Holds amplitude, synchronous phase, absolute phase, relative phase of
-        cavities.
+        cavities, phase acceptance, energy acceptance.
     beam_parameters :
         Holds emittance, Twiss parameters, envelopes in the various phase
         spaces.
