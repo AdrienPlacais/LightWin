dbe5e63d4a56bedc8f03cb2343687710f392554c
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-08-22 08:36:24 +0200

    added function to plot evolution of objective norm

--- src/lightwin/scripts/optimization_history.py
+++ src/lightwin/scripts/optimization_history.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Provide functions to study optimization history."""
+import logging
+from collections.abc import Sequence
 from pathlib import Path
 from typing import Literal
 
@@ -12,7 +14,7 @@ from matplotlib.axis import Axis
 def load(
     folder: Path, flag_constraints: bool = False
 ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    """Load the threee optimization history files in ``folder``."""
+    """Load the three optimization history files in ``folder``."""
     settings = pd.read_csv(folder / "settings.csv")
     objectives = pd.read_csv(folder / "objectives.csv")
 
@@ -25,15 +27,29 @@ def load(
 def get_optimization_objective_names(
     objectives: pd.DataFrame,
 ) -> tuple[list[str], list[str]]:
-    """Get the columns corresponding to optimization objectives.
+    """
+    Separate data from :class:`.Objective` and from :class:`.SimulationOutput`.
 
-    Also return columns taken from simulation outputs.
+    We expect that objectives have a ``|`` in their name, simulation outputs do
+    not.
 
     """
     cols = objectives.columns
-    opti_cols = [col for col in cols if "|" not in col]
+    opti_cols = [col for col in cols if "|" in col]
     simulation_output_cols = [col for col in cols if col not in opti_cols]
     return opti_cols, simulation_output_cols
+
+
+def add_objective_norm(
+    objectives: pd.DataFrame,
+    opti_cols: list[str],
+    norm_name: str = "Objectives norm",
+) -> tuple[pd.DataFrame, list[str]]:
+    """Compute norm of objectives and add it to the df."""
+    squared = (objectives[col] ** 2 for col in opti_cols)
+    objectives = objectives.assign(**{norm_name: np.sqrt(sum(squared))})
+    opti_cols.append(norm_name)
+    return objectives, opti_cols
 
 
 def plot_optimization_objectives(
@@ -59,19 +75,27 @@ def plot_optimization_objectives(
         **kwargs,
     )
     fig = plt.gcf()
+    assert fig.canvas.manager is not None
     fig.canvas.manager.set_window_title("objectives")
     return axis
 
 
-def _qty_sim_output(column_name: str) -> str:
+def _extract_qty_nature(column_name: str) -> str:
     """Get the quantity that is stored in the column ``column_name``.
 
-    It is expected that the header of the column is ``qty @ position``; it only
-    works for the quantites taken from :class:`.SimulationOutput` and written
-    in the ``objectives.csv``.
+    It is expected that the header of the column is ``qty @ position``.
 
     """
     return column_name.split("@")[0].strip()
+
+
+def _extract_qty_pos(column_name: str) -> str:
+    """Get where was evaluated what is stored in the column ``column_name``.
+
+    It is expected that the header of the column is ``qty @ position``.
+
+    """
+    return column_name.split("@")[1].split("(")[0].strip()
 
 
 def _post_treat(
@@ -96,7 +120,7 @@ def _post_treat(
     return treated_df, ylabel
 
 
-def plot_additional_objectives(
+def plot_simulation_outputs(
     objectives: pd.DataFrame,
     simulation_output_cols: list[str],
     subplots: bool = False,
@@ -107,14 +131,14 @@ def plot_additional_objectives(
     """Plot evolution of additional objectives."""
     do_not_logify = ("phi_s", "v_cav_mv")
     set_of_quantities = {
-        _qty_sim_output(col) for col in simulation_output_cols
+        _extract_qty_nature(col) for col in simulation_output_cols
     }
     axis = []
     for quantity in set_of_quantities:
         cols_to_plot = [
             col
             for col in simulation_output_cols
-            if _qty_sim_output(col) == quantity
+            if _extract_qty_nature(col) == quantity
         ]
         actual_logy = logy if quantity not in do_not_logify else None
 
@@ -140,24 +164,119 @@ def plot_additional_objectives(
     return axis
 
 
-def main(folder: Path) -> pd.DataFrame:
-    """Provide an example of complete study."""
-    variables, objectives, constants = load(folder)
+def identify_pareto(df: pd.DataFrame, objectives: list[str]) -> pd.DataFrame:
+    """Get the Pareto front."""
+    data = df[objectives].values
 
+    for obj in objectives:
+        max_abs = df[obj].abs().max()
+        df[obj + "_norm"] = df[obj] / max_abs
+
+    normalized_objectives = [obj + "_norm" for obj in objectives]
+
+    is_efficient = np.ones(data.shape[0], dtype=bool)
+    for i, obj_values in enumerate(data):
+        if is_efficient[i]:
+            # Calculate absolute values since we want to be close to zero
+            abs_values = np.abs(data)
+            # Check if any other point dominates the current point
+            is_dominated = np.all(
+                abs_values <= np.abs(obj_values), axis=1
+            ) & np.any(abs_values < np.abs(obj_values), axis=1)
+            is_efficient[is_dominated] = False
+    pareto = df[is_efficient]
+    pareto["distance"] = np.linalg.norm(pareto[normalized_objectives], axis=1)
+
+    return pareto
+
+
+def plot_solutions_3d(
+    df: pd.DataFrame, objective_names: Sequence[str], pareto: pd.DataFrame
+) -> None:
+    """Represent the solutions in 3d."""
+    if len(objective_names) != 3:
+        logging.warning(f"Cannot 3D plot non-3D {objective_names = }")
+        return
+    w_kin = objective_names[0]
+    phi_abs = objective_names[1]
+    mzdelta = objective_names[2]
+
+    # Plotting all solutions
+    fig = plt.figure(figsize=(10, 7))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(
+        df[w_kin],
+        df[phi_abs],
+        df[mzdelta],
+        color="blue",
+        label="All Solutions",
+        s=1.0,
+    )
+
+    # Highlight Pareto front solutions
+    ax.scatter(
+        pareto[w_kin],
+        pareto[phi_abs],
+        pareto[mzdelta],
+        color="red",
+        label="Pareto Front",
+        s=5.0,
+    )
+    best_solution = pareto.loc[pareto["distance"].idxmin()]
+    ax.scatter(
+        best_solution[w_kin],
+        best_solution[phi_abs],
+        best_solution[mzdelta],
+        color="green",
+        label="Best",
+        s=20.0,
+    )
+
+    ax.scatter(0, 0, 0, color="k", label="Ideal", s=20)
+
+    ax.set_xlabel("w_kin")
+    ax.set_ylabel("phi_abs")
+    ax.set_zlabel("Mzdelta")
+    ax.set_title("Pareto Front Visualization")
+    ax.legend()
+
+
+def main(
+    folder: Path,
+    plot_objectives: bool = True,
+    plot_objective_norm: bool = True,
+    plot_so: bool = False,
+    plot_objectives_3d: bool = True,
+) -> pd.DataFrame:
+    """Provide an example of complete study."""
     kwargs = {"grid": True}
+    _, objectives, _ = load(folder)
     opti_cols, simulation_output_cols = get_optimization_objective_names(
         objectives
     )
-    plot_optimization_objectives(
-        objectives, opti_cols, subplots=False, logy=True, **kwargs
+    norm_name = "Objectives norm"
+    objectives, opti_cols = add_objective_norm(
+        objectives, opti_cols, norm_name=norm_name
     )
-    plot_additional_objectives(
-        objectives,
-        simulation_output_cols,
-        logy=False,
-        post_treat="relative difference",
-        **kwargs,
-    )
+    if plot_objectives:
+        plot_optimization_objectives(
+            objectives, opti_cols, subplots=True, logy=True, **kwargs
+        )
+    if plot_objective_norm:
+        objectives.plot(y=norm_name, logy=True, **kwargs)
+
+    if plot_so:
+        plot_simulation_outputs(
+            objectives,
+            simulation_output_cols,
+            logy=True,
+            post_treat="relative difference",
+            **kwargs,
+        )
+
+    if plot_objectives_3d:
+        pareto = identify_pareto(objectives, opti_cols)
+        plot_solutions_3d(objectives, opti_cols, pareto)
     return objectives
 
 
