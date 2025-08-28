ea9cfbcdeab918c06abe11b34dbb3af3ad9d7611
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-08-28 10:51:19 +0200

    refactored Objective in order to be more DRY

--- src/lightwin/optimisation/objective/objective.py
+++ src/lightwin/optimisation/objective/objective.py
@@ -2,7 +2,7 @@
 
 import logging
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from collections.abc import Collection
 from typing import Any, Self
 
 from numpy.typing import NDArray
@@ -12,48 +12,143 @@ from lightwin.beam_calculation.simulation_output.simulation_output import (
 )
 from lightwin.core.beam_parameters.helper import mismatch_from_arrays
 from lightwin.util.typing import (
+    GETTABLE_BEAM_PARAMETERS,
     GETTABLE_BEAM_PARAMETERS_T,
     GETTABLE_SIMULATION_OUTPUT,
     GETTABLE_SIMULATION_OUTPUT_T,
 )
 
 
-@dataclass
 class Objective(ABC):
-    """Hold an objective and methods to evaluate it.
+    """Hold an objective and methods to evaluate it."""
 
-    Parameters
-    ----------
-    name :
-        A short string to describe the objective and access to it.
-    weight :
-        A scaling constant to set the weight of current objective.
-    descriptor :
-        A longer string to explain the objective.
-    ideal_value :
-        The ideal value or range of values that we should tend to.
+    #: List of authorized values for the ``get_key``. Checked by the
+    #: :meth:`._check_get_arguments` method
+    _gettable: Collection[str] = GETTABLE_SIMULATION_OUTPUT
 
-    """
+    def __init__(
+        self,
+        name: str,
+        weight: float,
+        get_key: GETTABLE_SIMULATION_OUTPUT_T,
+        get_kwargs: dict[str, Any],
+        ideal_value: float | tuple[float, float],
+        descriptor: str | None = None,
+    ) -> None:
+        """Hold an objective and methods to evaluate it.
 
-    name: str
-    weight: float
-    descriptor: str | None = None
-    ideal_value: Any | None = None
+        Parameters
+        ----------
+        name :
+            A short string to describe the objective and access to it.
+        weight :
+            A scaling constant to set the weight of current objective.
+        get_key :
+            Name of the quantity to get.
+        get_kwargs :
+            Keyword arguments for the :meth:`.SimulationOutput.get` method. We
+            do not enforce its validity, but in general you will want to define
+            the keys ``elt`` and ``pos``. If objective concerns a phase, you
+            may want to precise the ``to_deg`` key. You also should explicit
+            the ``to_numpy`` key.
+        ideal_value :
+            The ideal value or range of values that we should tend to.
+        descriptor :
+            A longer string to explain the objective.
 
-    def __post_init__(self) -> None:
-        """Avoid line jumps in the descriptor."""
-        if self.descriptor is None:
-            self.descriptor = ""
-            return
-        self.descriptor = " ".join(self.descriptor.split())
+        """
+        get_key, get_kwargs = self._check_get_arguments(get_key, get_kwargs)
+        self.name: str = name
+        self.weight: float = weight
+        self.get_key: GETTABLE_SIMULATION_OUTPUT_T = get_key
+        self.get_kwargs: dict[str, Any] = get_kwargs
+        self.ideal_value: Any = ideal_value
+        self.descriptor = " ".join((descriptor or "").split())
 
-    @abstractmethod
     def __str__(self) -> str:
-        """Output info on what is this objective about."""
+        """Give objective information value."""
+        message = self.base_str()
+        if isinstance(self.ideal_value, float):
+            message += f"{self.ideal_value:+.14e}"
+            return message
+        if isinstance(self.ideal_value, tuple):
+            message += (
+                f"{self.ideal_value[0]:+.2e} ~ {self.ideal_value[1]:+.2e}"
+            )
+            return message
+        if self.ideal_value is None:
+            message += f"{'None': ^21}"
+            return message
 
-    @abstractmethod
+        return message
+
     def base_str(self) -> str:
         """Tell nature and position of objective."""
+        message = f"{self.get_key:>23}"
+
+        elt = str(self.get_kwargs.get("elt", "NA"))
+        message += f" @elt {elt:>5}"
+
+        pos = str(self.get_kwargs.get("pos", "NA"))
+        message += f" ({pos:>3}) | {self.weight:>5} | "
+        return message
+
+    def _value_getter(
+        self,
+        simulation_output: SimulationOutput,
+        handle_missing_elt: bool = False,
+    ) -> float:
+        """Get desired value using :meth:`.SimulationOutput.get` method.
+
+        .. seealso::
+            :func:`.simulation_output.factory._element_to_index`
+
+        Parameters
+        ----------
+        simulation_output :
+            Object to ``get`` ``self.get_key`` from.
+        handle_missing_elt :
+            Automatically look for an equivalent :class:`.Element` when the
+            current one is not in :class:`.SimulationOutput`. Set it to
+            ``True`` when calculating reference value (reference
+            :class:`.Element` is not in compensating list of elements).
+
+        """
+        return simulation_output.get(
+            self.get_key,
+            **self.get_kwargs,
+            handle_missing_elt=handle_missing_elt,
+        )
+
+    def _check_get_arguments(
+        self,
+        get_key: GETTABLE_SIMULATION_OUTPUT_T,
+        get_kwargs: dict[str, Any],
+    ) -> tuple[GETTABLE_SIMULATION_OUTPUT_T, dict[str, Any]]:
+        """Check validity of ``get_args``, ``get_kwargs``.
+
+        In general, residuals evaluation relies on a
+        :meth:`.SimulationOutput.get` method. This method uses ``get_args`` and
+        ``get_kwargs``; we perform here some basic checks.
+
+        """
+        if get_key not in self._gettable:
+            logging.warning(
+                f"{get_key = } may not be gettable by SimulationOutput.get "
+                f"method. Authorized values are:\n{self._gettable = }"
+            )
+
+        advised_keys = ["elt", "pos", "to_numpy"]
+        if "phi" in get_key:
+            advised_keys.append("to_deg")
+        for key in advised_keys:
+            if key in get_kwargs:
+                continue
+            logging.warning(
+                f"{key = } is recommended to avoid undetermined behavior but "
+                "was not found."
+            )
+        return get_key, get_kwargs
 
     @staticmethod
     def str_header() -> str:
@@ -63,6 +158,15 @@ class Objective(ABC):
         return header
 
     @abstractmethod
+    def _compute_residuals(self, *args, **kwargs) -> float:
+        """Compute residual (loss), for a given value.
+
+        In general, you will want to call this function from
+        :meth:`.Objective.evaluate`.
+
+        """
+        raise NotImplementedError
+
     def evaluate(self, simulation_output: SimulationOutput) -> float:
         """Compute residuals of this objective.
 
@@ -77,43 +181,58 @@ class Objective(ABC):
             ``self.name``, scaled by ``self.weight``.
 
         """
+        value = self._value_getter(simulation_output)
+        return self._compute_residuals(value)
 
-    def _compute_residuals(self, *args, **kwargs) -> float:
-        """Compute residual (loss), for a given value.
 
-        In general, you will want to call this function from
-        :meth:`.Objective.evaluate`.
+class RetrieveArbitrary(Objective):
+    """Retrieve arbitrary value given by user."""
 
-        """
-        raise NotImplementedError
-
-    def _check_get_arguments(
-        self, get_key: str, get_kwargs: dict[str, Any]
+    def __init__(
+        self,
+        name: str,
+        weight: float,
+        get_key: GETTABLE_SIMULATION_OUTPUT_T,
+        get_kwargs: dict[str, Any],
+        ideal_value: float,
+        descriptor: str | None = None,
     ) -> None:
-        """Check validity of ``get_args``, ``get_kwargs``.
+        """
+        Set complementary :meth:`.SimulationOutput.get` flags, reference value.
 
-        In general, residuals evaluation relies on a
-        :meth:`.SimulationOutput.get` method. This method uses ``get_args`` and
-        ``get_kwargs``; we perform here some basic checks.
+        Parameters
+        ----------
+        name :
+            A short string to describe the objective and access to it.
+        weight :
+            A scaling constant to set the weight of current objective.
+        get_key :
+            Name of the quantity to get.
+        get_kwargs :
+            Keyword arguments for the :meth:`.SimulationOutput.get` method. We
+            do not check its validity, but in general you will want to define
+            the keys ``elt`` and ``pos``. If objective concerns a phase, you
+            may want to precise the ``to_deg`` key. You also should explicit
+            the ``to_numpy`` key.
+        ideal_value :
+            The value to retrieve.
+        descriptor :
+            A longer string to explain the objective.
 
         """
-        if get_key not in GETTABLE_SIMULATION_OUTPUT:
-            logging.warning(
-                f"{get_key = } may not be gettable by SimulationOutput.get "
-                "method. Authorized values are:\n"
-                f"{GETTABLE_SIMULATION_OUTPUT = }"
-            )
+        self.ideal_value: float
+        super().__init__(
+            name=name,
+            weight=weight,
+            get_key=get_key,
+            get_kwargs=get_kwargs,
+            descriptor=descriptor,
+            ideal_value=ideal_value,
+        )
 
-        advised_keys = ["elt", "pos", "to_numpy"]
-        if "phi" in get_key:
-            advised_keys.append("to_deg")
-        for key in advised_keys:
-            if key in get_kwargs:
-                continue
-            logging.warning(
-                f"{key = } is recommended to avoid undetermined behavior but "
-                "was not found."
-            )
+    def _compute_residuals(self, value: float) -> float:
+        """Compute residuals, that we want to minimize."""
+        return self.weight * abs(value - self.ideal_value)
 
 
 class MinimizeDifferenceWithRef(Objective):
@@ -152,72 +271,16 @@ class MinimizeDifferenceWithRef(Objective):
             A longer string to explain the objective.
 
         """
-        self._check_get_arguments(get_key, get_kwargs)
-        self.get_key: GETTABLE_SIMULATION_OUTPUT_T = get_key
-        self.get_kwargs = get_kwargs
         self.ideal_value: float
         super().__init__(
-            name,
-            weight,
+            name=name,
+            weight=weight,
+            get_key=get_key,
+            get_kwargs=get_kwargs,
             descriptor=descriptor,
             ideal_value=self._value_getter(reference, handle_missing_elt=True),
         )
         self._check_ideal_value()
-
-    def base_str(self) -> str:
-        """Tell nature and position of objective."""
-        message = f"{self.get_key:>23}"
-
-        elt = str(self.get_kwargs.get("elt", "NA"))
-        message += f" @elt {elt:>5}"
-
-        pos = str(self.get_kwargs.get("pos", "NA"))
-        message += f" ({pos:>3}) | {self.weight:>5} | "
-        return message
-
-    def __str__(self) -> str:
-        """Give objective information value."""
-        message = self.base_str()
-        if isinstance(self.ideal_value, float):
-            message += f"{self.ideal_value:+.14e}"
-            return message
-        if isinstance(self.ideal_value, tuple):
-            message += (
-                f"{self.ideal_value[0]:+.2e} ~ {self.ideal_value[1]:+.2e}"
-            )
-            return message
-        if self.ideal_value is None:
-            message += f"{'None': ^21}"
-            return message
-
-        return message
-
-    def _value_getter(
-        self,
-        simulation_output: SimulationOutput,
-        handle_missing_elt: bool = False,
-    ) -> float:
-        """Get desired value using :meth:`.SimulationOutput.get` method.
-
-        .. seealso::
-            :func:`.simulation_output.factory._element_to_index`
-
-        Parameters
-        ----------
-        simulation_output :
-            Object to ``get`` ``self.get_key`` from.
-        handle_missing_elt :
-            Automatically look for an equivalent :class:`.Element` when the
-            current one is not in :class:`.SimulationOutput`. Set it to
-            ``True`` when calculating reference value (reference
-            :class:`.Element` is not in compensating list of elements).
-
-        """
-        return simulation_output.get(
-            self.get_key,
-            **self.get_kwargs,
-            handle_missing_elt=handle_missing_elt,
-        )
 
     def _check_ideal_value(self) -> None:
         """Assert the the reference value is a float."""
@@ -227,11 +290,6 @@ class MinimizeDifferenceWithRef(Objective):
                 f"returned {self.ideal_value} instead of a float."
             )
 
-    def evaluate(self, simulation_output: SimulationOutput | float) -> float:
-        assert isinstance(simulation_output, SimulationOutput)
-        value = self._value_getter(simulation_output)
-        return self._compute_residuals(value)
-
     def _compute_residuals(self, value: float) -> float:
         """Compute residuals, that we want to minimize."""
         return self.weight * abs(value - self.ideal_value)
@@ -239,6 +297,8 @@ class MinimizeDifferenceWithRef(Objective):
 
 class MinimizeMismatch(Objective):
     """Minimize a mismatch factor."""
+
+    _gettable = GETTABLE_BEAM_PARAMETERS
 
     def __init__(
         self,
@@ -273,6 +333,22 @@ class MinimizeMismatch(Objective):
             A longer string to explain the objective.
 
         """
+        self.ideal_value: float
+        self.get_key: GETTABLE_BEAM_PARAMETERS_T
+        super().__init__(
+            name=name,
+            weight=weight,
+            get_key=get_key,
+            get_kwargs=get_kwargs,
+            descriptor=descriptor,
+            ideal_value=0.0,
+        )
+        self._twiss_ref = self._twiss_getter(reference)
+
+    def _check_get_arguments(
+        self, get_key: GETTABLE_SIMULATION_OUTPUT_T, get_kwargs: dict[str, Any]
+    ) -> tuple[GETTABLE_SIMULATION_OUTPUT_T, dict[str, Any]]:
+        """Add default values if necessary."""
         if "twiss" not in get_key:
             logging.warning(
                 "The get_key should contain 'twiss'. Taking 'twiss' and "
@@ -280,26 +356,7 @@ class MinimizeMismatch(Objective):
             )
             get_key = "twiss"
             get_kwargs["phase_space_name"] = "zdelta"
-        self._check_get_arguments(get_key, get_kwargs)
-        self.get_key: GETTABLE_BEAM_PARAMETERS_T = get_key
-        self.get_kwargs = get_kwargs
-        super().__init__(name, weight, descriptor=descriptor, ideal_value=0.0)
-        self._twiss_ref = self._twiss_getter(reference)
-
-    def base_str(self) -> str:
-        """Tell nature and position of objective."""
-        message = f"{self.name:>23}"
-
-        elt = str(self.get_kwargs.get("elt", "NA"))
-        message += f" @elt {elt:>5}"
-
-        pos = str(self.get_kwargs.get("pos", "NA"))
-        message += f" ({pos:>3}) | {self.weight:>5} | "
-        return message
-
-    def __str__(self) -> str:
-        """Give objective information value."""
-        return self.base_str() + f"{self.ideal_value:+.14e}"
+        return super()._check_get_arguments(get_key, get_kwargs)
 
     def _twiss_getter(self, simulation_output: SimulationOutput) -> NDArray:
         """Get desired value using :meth:`.SimulationOutput.get` method."""
@@ -354,12 +411,14 @@ class QuantityIsBetween(Objective):
             outside the limits. Currently not implemented.
 
         """
-        self._check_get_arguments(get_key, get_kwargs)
-        self.get_key: GETTABLE_SIMULATION_OUTPUT_T = get_key
-        self.get_kwargs = get_kwargs
         self.ideal_value: tuple[float, float]
         super().__init__(
-            name, weight, descriptor=descriptor, ideal_value=limits
+            name=name,
+            weight=weight,
+            get_key=get_key,
+            get_kwargs=get_kwargs,
+            ideal_value=limits,
+            descriptor=descriptor,
         )
         if loss_function is not None:
             logging.warning("Loss functions not implemented.")
@@ -429,31 +488,11 @@ class QuantityIsBetween(Objective):
             loss_function=loss_function,
         )
 
-    def base_str(self) -> str:
-        """Tell nature and position of objective."""
-        message = f"{self.get_key:>23}"
-
-        elt = str(self.get_kwargs.get("elt", "NA"))
-        message += f" @elt {elt:>5}"
-
-        pos = str(self.get_kwargs.get("pos", "NA"))
-        message += f" ({pos:>3}) | {self.weight:>5} | "
-        return message
-
     def __str__(self) -> str:
         """Give objective information value."""
         message = self.base_str()
         message += f"{self.ideal_value[0]:+.2e} ~ {self.ideal_value[1]:+.2e}"  # type: ignore
         return message
-
-    def _value_getter(self, simulation_output: SimulationOutput) -> float:
-        """Get desired value using :meth:`.SimulationOutput.get` method."""
-        return simulation_output.get(self.get_key, **self.get_kwargs)
-
-    def evaluate(self, simulation_output: SimulationOutput) -> float:
-        assert isinstance(simulation_output, SimulationOutput)
-        value = self._value_getter(simulation_output)
-        return self._compute_residuals(value)
 
     def _compute_residuals(self, value: float) -> float:
         """Compute residual for ``value`` with respect to the ideal interval.
