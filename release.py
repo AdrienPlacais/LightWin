d0a4d4880d85ec2d37f59483df361f806d67db6a
AdrienPlacais <28395365+AdrienPlacais@users.noreply.github.com>
2026-09-02 10:08:38 +0200

    rewrite regex to avoid precommit modifying it

--- release.py
+++ release.py
@@ -160,7 +160,10 @@ def _extract_changelog_section(
         if not found.
 
     """
-    pattern = rf"^## \[{re.escape(version)}\](?: -- (unreleased|\d{ 4}}-\d{ 2}}-\d{ 2}}))?$"
+    pattern = (
+        rf"^## \[{re.escape(version)}\]" r"(?: -- (unreleased|\d{4}-\d{2}-\d{2}))?$"
+    )
+
     lines = content.splitlines()
     for i, line in enumerate(lines):
         if match := re.match(pattern, line, re.IGNORECASE):
